40a32f42aeba3beb651f7ae676922591849c89cc
programmerShoaib <link2shoaib.2002@gmail.com>
2025-04-02 00:35:17 +0500

    the api is not working

--- script.js
+++ script.js
@@ -4,19 +4,56 @@ let dropdowns = document.querySelectorAll("#dropdowns select");
 let btn = document.querySelector("form button");
 let fromCurrency = document.querySelector("#from select");
 let toCurrency = document.querySelector("#to select");
-let msg = document.querySelector("#message");
-// let API_KEY = "a5d2e9d7d5b4c8d1e2f3c4d5e6f7c8d9";
-// let amount = document.getElementById("amount");
-// let from = document.getElementById("from");
-// let to = document.getElementById("to");
-// let result = document.getElementById("result");
+let result = document.querySelector("#result");
 
-// document.getElementById("convert").addEventListener("click", () => {
-//     BASE_URL = `https://api.exchangerate.host/convert?from=${from.value}&to=${to.value}`;
-//     fetch(BASE_URL)
-//         .then((response) => response.json())
-//         .then((data) => {
-//             result.value = (data.result * amount.value).toFixed(2);
-//         })
-//         .catch((error) => console.error(error));
-// });
+for (select of dropdowns) {
+    for (currencyCode in countryList) {
+        let option = document.createElement("option");
+        option.value = currencyCode;
+        option.innerHTML = countryList[currencyCode];
+        if (select.name === "from" && currencyCode === "USD") {
+            option.selected = "selected";
+        } else if (select.name === "to" && currencyCode === "PKR") {
+            option.selected = "selected";
+        }
+        select.appendChild(option);
+    }
+
+    select.addEventListener("change", (e) => {
+        upDateFlags(e.target);
+    });
+}
+
+const upDateFlags = (element) => {
+    let currencyCode = element.value;
+    let countryCode = countryList[currencyCode];
+    let flagImg = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    element.parentElement.querySelector("img").src = flagImg;
+}
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    updateExchangeRate();
+});
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector("form input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+      amtVal = 1;
+      amount.value = "1";
+    }
+    const URL = `${BASE_URL}/${fromCurrency.value.toLowerCase()}/${toCurrency.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurrency.value.toLowerCase()];
+  
+    let finalAmount = amtVal * rate;
+    result.innerText = `${amtVal} ${fromCurrency.value} = ${finalAmount} ${toCurrency.value}`;
+  };
+
+
+
+
+    
+    
