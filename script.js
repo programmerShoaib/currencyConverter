3b3ff36ffeaa2a2667d24e4cadc4a8ad5a83805a
programmerShoaib <link2shoaib.2002@gmail.com>
2025-04-01 21:58:40 +0500

    add some code

---
+++ script.js
@@ -0,0 +1,16 @@
+let BASE_URL = "https://2024-03-06.currency-api.pages.dev/v1/currencies/UYU.json";
+// let API_KEY = "a5d2e9d7d5b4c8d1e2f3c4d5e6f7c8d9";
+// let amount = document.getElementById("amount");
+// let from = document.getElementById("from");
+// let to = document.getElementById("to");
+// let result = document.getElementById("result");
+
+// document.getElementById("convert").addEventListener("click", () => {
+//     BASE_URL = `https://api.exchangerate.host/convert?from=${from.value}&to=${to.value}`;
+//     fetch(BASE_URL)
+//         .then((response) => response.json())
+//         .then((data) => {
+//             result.value = (data.result * amount.value).toFixed(2);
+//         })
+//         .catch((error) => console.error(error));
+// });
